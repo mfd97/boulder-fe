f4e03cc3ed5ea53584edf910240d4419a23bb79e
alhabajnouf-netizen <alhabajnouf@gmail.com>
2026-02-04 18:23:08 +0300

    create a quiz

---
+++ app/(protected)/(tabs)/quiz/CreateQuiz.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const CreateQuiz = () => {
+  return (
+    <View>
+      <Text>CreateQuiz</Text>
+    </View>
+  );
+};
+
+export default CreateQuiz;
+
+const styles = StyleSheet.create({});
